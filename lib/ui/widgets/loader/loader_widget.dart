97413ddcdcea8f91ebc341b570ccf826c2fbbdf1
KroGen <daniil.king333@gmail.com>
2024-08-15 11:34:53 +0300

    Refactoring LoaderPage.

--- lib/ui/widgets/loader/loader_widget.dart
+++ lib/ui/widgets/loader/loader_widget.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:themoviedb/domain/repositories/auth_repository.dart';
+import 'package:themoviedb/ui/widgets/authentication/authentication.dart';
 
 class LoaderWidget extends StatelessWidget {
-  const LoaderWidget({super.key});
+  final Widget authenticated;
+  final Widget unauthenticated;
+
+  const LoaderWidget({
+    super.key,
+    required this.authenticated,
+    required this.unauthenticated,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
+    return Scaffold(
+      body: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: (context, state) {
+        switch (state.status) {
+          case AuthenticationStatus.authenticated:
+            return authenticated;
+          case AuthenticationStatus.unauthenticated:
+            return unauthenticated;
+          case AuthenticationStatus.unknown:
+            return const Center(child: CircularProgressIndicator());
+        }
+      }),
     );
   }
 }
